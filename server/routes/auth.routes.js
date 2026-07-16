6ce4cf3dc6faec6bd99974ced2566b760338b765
Siddhesh Agare <siddheshagare432@gmail.com>
2026-07-16 17:57:48 +0530

    register user api completed

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import { testingApi } from '../controllers/auth.controllers.js'
+import { registerUser } from '../controllers/auth.controllers.js'
 const authRouter = express.Router()
 
 authRouter.get('/test', testingApi);
 
+authRouter.post('/register', registerUser);
 export default authRouter
