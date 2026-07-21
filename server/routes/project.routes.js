eb4571734931ce7e54b573cc81665ab8f1d4d68f
Siddhesh Agare <siddheshagare432@gmail.com>
2026-07-21 21:40:26 +0530

    project api is completed

---
+++ server/routes/project.routes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import isAuth from '../middleware/isAuth.js';
+import upload from '../middleware/multer.js';
+import { addProject, deleteProject, getAdminProjects } from '../controllers/project.controllers.js';
+const projectRouter = express.Router();
+
+projectRouter.post("/add", isAuth, upload.single('projectImage'), addProject);
+projectRouter.delete("/delete/:projectId", isAuth, upload.single('projectImage'), deleteProject);
+projectRouter.get("/get-admin-projects", isAuth, getAdminProjects);
+
+
+export default projectRouter;
